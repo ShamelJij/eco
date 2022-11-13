d2346150ad042c3257ea7eba4aeb044af396368b
shameljij <shamel.jij@gmail.com>
2022-11-13 23:12:19 +0100

    added requests (getSolar)

---
+++ public/archive2/app.js
@@ -0,0 +1,20 @@
+var dateTime = setInterval(function () {
+  myTimer();
+}, 1000);
+
+function myTimer() {
+  const weekday = new Date().toLocaleString("de-de", { weekday: "long" });
+  const utc = new Date().toUTCString();
+  const t = new Date();
+  document.getElementById("clock").innerHTML =
+    "&nbsp;" +
+    weekday +
+    " - " +
+    t.toLocaleDateString().split("/")[1] +
+    "." +
+    t.toLocaleDateString().split("/")[0] +
+    "." +
+    t.toLocaleDateString().split("/")[2] +
+    " - " +
+    t.toLocaleTimeString("de-de", { hour12: false });
+}
---
+++ public/requests.js
@@ -0,0 +1,35 @@
+export class requests{
+  //################################################################################
+  /**
+   * @param {string} method POST/PUT/DELETE/GET
+   * @param {string} url
+   */
+  sendHTTPRequest(method, url) {
+    let promise = new Promise((resolve, reject) => {
+      let xhr = new XMLHttpRequest();
+      xhr.open(method, url);
+      xhr.responseType = 'json';
+      xhr.onload = function () {
+        if (xhr.status != 200) {
+          // analyze HTTP status of the response
+          alert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
+        } else {
+          // show the result
+          console.log(`Done, got ${xhr.response.length} bytes`); // response is the server response
+          resolve(xhr.response);
+        }
+      };
+      xhr.send();
+    });
+    return promise;
+  }
+
+  //--------------------------------------------------------------------------------
+  /**
+   * GET /inventories or /persons or /locations
+   * @return {Array.<Objects>}
+   */
+  async getSolar() {
+    return this.sendHTTPRequest('GET', 'http://localhost:8080/solar/');
+  }
+  }
